0be1d68d78e8f846e9489b18870b4d898ab5e26d
RKikkas <roger.kikkas@gmail.com>
2019-04-20 15:52:05 +0300

    Episode 17

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,5 @@ Route::get('/projects/{project}/edit', 'ProjectsController@edit');
 Route::patch('/projects/{project}', 'ProjectsController@update');
 Route::delete('/projects/{project}', 'ProjectsController@destroy');
 */
+
+Route::patch('tasks/{task}', 'ProjectTasksController@update');
