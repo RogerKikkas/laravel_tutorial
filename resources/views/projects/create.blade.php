042bcf70a899f34029f350c9e307da5cc4b8d862
RKikkas <roger.kikkas@gmail.com>
2019-04-20 15:18:58 +0300

    Episode 15

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -5,16 +5,39 @@
 
     <form method="POST" action="/laravel_tutorial/public/projects">
         {{ csrf_field() }}
-        <div>
-            <input type="text" name="title" placeholder="Project title">
-        </div>
-        
-        <div>
-            <textarea name="description" placeholder="Project description"></textarea>
+
+        <div class="field">
+            <label class="label" for="title">Project Title</label>
+
+            <div class="control">
+                <input type="text" class="input {{ $errors->has('title') ? 'is-danger' : '' }}" name="title"
+                       placeholder="Project title" value="{{ old('title') }}"required>
+            </div>
         </div>
 
-        <div>
-            <button type="submit">Create Project</button>
+        <div class="field">
+            <label class="label" for="description">Project Description</label>
+
+            <div class="control">
+                <textarea name="description" class="textarea {{ $errors->has('description') ? 'is-danger' : '' }}"
+                          placeholder="Project description" required>{{ old('description') }}</textarea>
+            </div>
         </div>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Create Project</button>
+            </div>
+        </div>
+
+        @if ($errors->any())
+            <div class="notification is-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </form>
 @endsection
