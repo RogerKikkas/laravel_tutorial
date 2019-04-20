b0d010d516cdb7eb0a0d46bee999ce199e715c75
RKikkas <roger.kikkas@gmail.com>
2019-04-20 16:37:27 +0300

    Episode 19

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -10,18 +10,16 @@ class ProjectTasksController extends Controller
 {
     public function store(Project $project)
     {
-        $attributes = request()->validate(['description' => 'required']);
-
-        $project->addTask($attributes);
+        $project->addTask(
+            request()->validate(['description' => 'required'])
+        );
 
         return back();
     }
 
     public function update(Task $task)
     {
-        $task->update([
-            'completed' => request()->has('completed')
-        ]);
+        request()->has('completed') ? $task->complete() : $task->incomplete();
 
         return back();
     }
